bbc3a5d0727e3ff5767b8e4a8f10c581df5dc719
datpro903-dau-24ct1 <datpro903@gmail.com>
2026-01-28 13:09:54 +0700

    DAU

---
+++ Teemo Attacking.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def findPoisonedDuration(self, timeSeries, duration):
+        t=0
+        for i in range(len(timeSeries)-1):
+            d=timeSeries[i+1]-timeSeries[i]
+            t+= min(d,duration)
+        return t+duration
+        
