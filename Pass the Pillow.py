a3fc4ebee056a515dfd32455139b62a6c2e2fdd4
datpro903-dau-24ct1 <datpro903@gmail.com>
2026-02-23 15:41:56 +0700

    DAU

---
+++ Pass the Pillow.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def passThePillow(self, n, time):
+        t = time % (2*(n-1))
+        if t < n:
+            return t+1
+        else:
+            return 2*(n-1)-t+1     
