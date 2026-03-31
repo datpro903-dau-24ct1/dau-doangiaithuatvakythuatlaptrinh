3529b624ef7e0b63f70c85a7c76390eabdc204a5
datpro903-dau-24ct1 <datpro903@gmail.com>
2026-03-31 07:41:20 +0700

    DAU

---
+++ Count Operations to Obtain Zero.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def countOperations(self, num1, num2):
+        count = 0
+        while num1 != 0 and num2 != 0:
+            if num1 >= num2:
+                count += num1 // num2
+                num1 %= num2
+            else:
+                count += num2 // num1
+                num2 %= num1
+        return count
+        
